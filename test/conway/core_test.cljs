13d0764495d6b26903626cb3ed5b6f0d7b64c424
Bobby Towers <porkostomus@mail.com>
2019-06-14 15:31:21 -0700

    remove multiply fn

--- test/conway/core_test.cljs
+++ test/conway/core_test.cljs
@@ -1,10 +1,10 @@
 (ns conway.core-test
     (:require
      [cljs.test :refer-macros [deftest is testing]]
-     [conway.core :refer [multiply]]))
+     [conway.core :refer []]))
 
-(deftest multiply-test
-  (is (= (* 1 2) (multiply 1 2))))
+;(deftest multiply-test
+;  (is (= (* 1 2) (multiply 1 2))))
 
-(deftest multiply-test-2
-  (is (= (* 75 10) (multiply 10 75))))
+;(deftest multiply-test-2
+;  (is (= (* 75 10) (multiply 10 75))))
