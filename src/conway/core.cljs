b6d306a389abd229086bea452cd9c339b0d2cab3
Bobby Towers <porkostomus@mail.com>
2019-06-13 22:05:41 -0700

    glider gun, player, and input field

--- src/conway/core.cljs
+++ src/conway/core.cljs
@@ -5,24 +5,27 @@
 
 (defn multiply [a b] (* a b))
 
-(defonce app-state (atom {:status "not started"}))
+(def status (atom "not started"))
 
-(defn get-app-element []
-  (gdom/getElement "app"))
+(def world-size (atom 10))
 
-(def world-size 10)
+(defn size-input [value]
+  [:input {:type "number"
+           :value @world-size
+           :on-change #(reset! world-size (-> % .-target .-value))}])
 
-(defn wrap-coordinate [n]
-  (mod n world-size))
+(defn size-field []
+    (fn []
+      [:div
+       [:p "World size: " [size-input @world-size]]]))
 
 (defn wrap-square [[x y]]
-  [(wrap-coordinate x) (wrap-coordinate y)])
+  [(mod x (js/parseInt @world-size)) (mod y (js/parseInt @world-size))])
 
 (defn wrap-squares [lives]
   (map wrap-square lives))
 
-(def lives
-  (atom #{}))
+(def lives (atom #{}))
 
 (defn neighbors [[x y]]
   (for [dx [-1 0 1] dy [-1 0 1]
@@ -30,21 +33,19 @@
     [(+ x dx) (+ y dy)]))
 
 (defn step [lives]
-  (set (for [[pos live-neighbors] (frequencies (mapcat neighbors lives))
+  (set (for [[pos live-neighbors]
+             (frequencies (mapcat neighbors lives))
              :when (or (= 3 live-neighbors)
                        (and (contains? lives pos)
                             (= 2 live-neighbors)))]
          pos)))
-
-(defn step! []
-  (reset! lives (step @lives)))
 
 (defn rect-cell [x y]
   [:rect.cell
    {:x (+ 0.05 x) :width 0.9
     :y (+ 0.05 y) :height 0.9
     :fill "white"
-    :stroke-width 0.025
+    :stroke-width 0.05
     :stroke "black"}])
 
 (defn dead [x y]
@@ -56,7 +57,7 @@
     :y (+ 0.05 y)
     :on-click
     (fn click-square [e]
-      (if (= (:status @app-state) "not started")
+      (if (= @status "not started")
         (swap! lives conj [x y])
         (js/alert "Hands off! You are not God.")))}])
 
@@ -69,62 +70,105 @@
     :y (+ 0.05 y)
     :on-click
     (fn click-square [e]
-      (if (= (:status @app-state) "not started")
+      (if (= @status "not started")
         (swap! lives disj [x y])
         (js/alert "Ouch!")))}])
 
 (defn render-board []
   (into
     [:svg.board
-      {:view-box (str "0 0 " world-size " " world-size)
+      {:view-box (str "0 0 " @world-size " " @world-size)
        :shape-rendering "auto"
        :style {:max-height "500px"}}]
-           (for [x (range world-size)
-                 y (range world-size)]
+           (for [x (range @world-size)
+                 y (range @world-size)]
              [:g
               [rect-cell x y]
                 (if (some #{[x y]} (wrap-squares @lives))
                 [life x y]
                 [dead x y])])))
 
+(def glider-gun
+  {:name "Glider gun"
+   :lives #{[27 18] [32 13] [25 13] [28 15] [36 16]
+            [25 19] [28 17] [47 13] [28 16] [33 14]
+            [34 12] [36 12] [27 14] [24 19] [26 16]
+            [23 18] [34 16] [32 15] [12 15] [33 13]
+            [36 17] [46 14] [29 16] [36 11] [12 16]
+            [22 15] [23 14] [24 13] [22 16] [22 17]
+            [32 14] [33 15] [13 16] [47 14] [13 15] [46 13]}})
+
+(def penta-decathlon
+  {:name "Penta-decathlon"
+   :lives
+   #{[5 7] [4 3] [3 4] [5 3] [3 2] [4 6] [5 5]
+     [5 6] [5 2] [3 1] [5 1] [4 5] [3 3] [5 4]
+     [3 5] [4 8] [4 1] [3 6] [5 8] [3 8] [4 4] [3 7]}})
+
+(def glider
+  {:name "Glider"
+   :lives #{[3 4] [4 4] [5 4] [5 3] [4 2]}})
+
+(def spaceship
+  {:name "Spaceship"
+   :lives #{[4 3] [5 3] [7 2] [5 5] [8 3] [7 4] [5 4]
+            [6 5] [4 4] [7 3] [6 2] [6 4]}})
+
+(defn config-button [m]
+  [:button
+   {:on-click
+    (fn step-click [e]
+      (reset! lives (:lives m)))}
+      (:name m)])
+
+(def step-timer (atom (js/setInterval #(swap! lives step) 100000)))
+
+(defn start-timer! []
+  (reset! step-timer (js/setInterval #(swap! lives step) 300)))
+
+(defn stop-timer! [timer]
+  (js/clearInterval timer))
+
 (defn game []
   [:center
    [:h1 "Conway's Game of Life"]
-   [:div [:button
-    {:on-click
-     (fn step-click [e]
-       (swap! lives step)
-       (swap! app-state assoc-in [:status] "started"))}
-    "Step"]
+   [size-field]
+   [:div
+    [:button
+     {:on-click
+      (fn step-click [e]
+        (swap! lives step)
+        (reset! status "started"))}
+     "Step"]
+    [:button
+     {:on-click
+      (fn step-click [e]
+        (start-timer!)
+        (reset! status "started"))}
+     "Play"]
+    [:button
+     {:on-click
+      (fn step-click [e]
+        (stop-timer! @step-timer)
+        (reset! status "started"))}
+     "Stop"]
     [:button
      {:on-click
       (fn step-click [e]
         (reset! lives #{})
-        (swap! app-state assoc-in [:status] "not started"))}
+        (reset! status "not started"))}
      "Reset"]]
-   [:div [:button
-    {:on-click
-     (fn step-click [e]
-       (reset! lives
-         #{[3 4] [4 4] [5 4] [5 3] [4 2]}))}
-    "Glider"]
-    [:button
-     {:on-click
-      (fn step-click [e]
-        (reset! lives
-          #{[4 3] [5 3] [7 2] [5 5] [8 3] [7 4] [5 4]
-            [6 5] [4 4] [7 3] [6 2] [6 4]}))}
-     "Spaceship"]
-   [:button
-    {:on-click
-     (fn step-click [e]
-       (reset! lives
-         #{[5 7] [4 3] [3 4] [5 3] [3 2] [4 6] [5 5]
-           [5 6] [5 2] [3 1] [5 1] [4 5] [3 3] [5 4]
-           [3 5] [4 8] [4 1] [3 6] [5 8] [3 8] [4 4] [3 7]}))}
-    "Penta-decathlon"]]
+   [:div
+    (config-button glider)
+    (config-button spaceship)
+    (config-button penta-decathlon)
+    (config-button glider-gun)]
    [:div [render-board]]
-   [:p (str "Current residents:" @lives)]])
+   [:p (str "Current residents: " @lives)]
+   [:p (str "Wrapped to: " (wrap-squares @lives))]])
+
+(defn get-app-element []
+  (gdom/getElement "app"))
 
 (defn mount [el]
   (reagent/render-component [game] el))
